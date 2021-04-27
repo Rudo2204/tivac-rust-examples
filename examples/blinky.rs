6b92c7cf09da649aaf62705079b0fc9d1d0493bc
Rudo2204 <rudo2204@gmail.com>
2021-04-27 19:25:10 +0700

    Update blinky with alloc

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,12 +1,15 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
 extern crate embedded_hal;
 extern crate stellaris_launchpad;
 extern crate tm4c123x_hal;
 
+use core::alloc::Layout;
 use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::digital::v2::OutputPin;
+use stellaris_launchpad::board;
 
 #[no_mangle]
 pub fn stellaris_main(mut board: stellaris_launchpad::board::Board) {
@@ -21,4 +24,9 @@ pub fn stellaris_main(mut board: stellaris_launchpad::board::Board) {
         board.led_green.set_low().unwrap();
         delay.delay_ms(500u32);
     }
+}
+
+#[alloc_error_handler]
+fn oom(_: Layout) -> ! {
+    board::panic();
 }
